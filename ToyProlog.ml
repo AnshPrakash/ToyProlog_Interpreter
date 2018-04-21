a81a9141e8b55c8f6a72819dfd60c2ad99c8eec6
AnshPrakash <33067038+AnshPrakash@users.noreply.github.com>
2018-04-21 17:03:08 +0530

    Add files via upload

---
+++ ToyProlog.ml
@@ -0,0 +1,15 @@
+
+
+type variable=X of string;;
+type sym= Sym of string*int;; (*int is the arrity*)
+type atom= P_sym of sym*(term list);;
+type term= Const of int| V of variable|Func of atom;;
+type body=Body of atom list;;
+type head=Head of atom;;
+type clause =Fact of head|Rule of head*body;;
+type program= Prog of clause list;;
+type goal=Goal of atom list;;
+
+
+
+
